7c9bc44b97889e31efa70a9847fee01f72e61f7b
Igor Diogo <igordiogo1314@gmail.com>
2026-02-25 17:21:48 -0300

    Simulação da partida

--- js/app.js
+++ js/app.js
@@ -52,6 +52,23 @@ function carregarTimes(){
     }
 }
 
+function carregarTimesPartida(){
+    timesSelect = [document.querySelector("#time1"), 
+        document.querySelector("#time2")]
+
+    for(let time of listaTimes){
+        for(let i=0;i<2;i++){
+            let option = document.createElement("option");
+            option.textContent = time.nome;
+            option.value = time.id;
+            option.style.backgroundColor = time.cor1;
+            option.style.color = time.cor2;
+
+            timesSelect[i].appendChild(option);
+        }   
+    }
+}
+
 function abrirMenu(){
     const menu = document.querySelector("menu");
     menu.classList.add("menuAberto");
@@ -60,6 +77,10 @@ function abrirMenu(){
 function fecharMenu(){
     const menu = document.querySelector("menu");
     menu.classList.remove("menuAberto");
+}
+
+function irParaPagina(pagina){
+    window.location.href = pagina;
 }
 
 function abrirFormulario(){
@@ -87,6 +108,10 @@ function registrarTime(){
     }
 
     const time = obterDadosFormulario(idTime);
+
+    if(!time){
+        return alert("Não deixe campos incompletos.");
+    }
     
     listaTimes.push(time);
     localStorage.setItem("listaTimes", JSON.stringify(listaTimes));
@@ -134,9 +159,17 @@ function obterDadosFormulario(idTime){
         const posJogador = document.querySelectorAll(".posicaoSelect")[i].value;
         const nomeJogador = document.querySelectorAll(".nomePlayer")[i].value;
         const overJogador = document.querySelectorAll(".overPlayer")[i].value;
+
+        if(posJogador=="nenhuma" || !nomeJogador || !overJogador){
+            return null;
+        }
         
         listajogadores.push({pos: posJogador, nome: nomeJogador, over: overJogador});
     }    
+
+    if(!nomeTime || !cor1Time || !cor2Time){
+        return null;
+    }
     
     const time = {
         id: idTime,
@@ -151,7 +184,7 @@ function obterDadosFormulario(idTime){
 
 function carregarFormularioEdicao(id){
     let time = listaTimes.find(item => item.id == id);
-
+    
     abrirFormulario();  
     document.querySelector("#nomeTime-formulario").value = time.nome;
     document.querySelector("#cor1-formulario").value = time.cor1;
@@ -163,7 +196,111 @@ function carregarFormularioEdicao(id){
         document.querySelectorAll(".nomePlayer")[i].value = jogador.nome;
         document.querySelectorAll(".overPlayer")[i].value = jogador.over;
     }    
-
+    
     const btnConfirmar = document.querySelector("#btnConfirmar-formulario");
     btnConfirmar.setAttribute("onclick", "editarTime("+id+")");
+}
+
+function chamarSimulacao(){
+    const iniciarPartidaBtn = document.querySelector("#iniciarPartidaBtn");
+    iniciarPartidaBtn.style.pointerEvents = "none";
+
+    const idTime1 = document.querySelector("#time1").value; 
+    const idTime2 = document.querySelector("#time2").value;
+    
+    let time1 = listaTimes.find(item => item.id == idTime1);
+    let time2 = listaTimes.find(item => item.id == idTime2);
+
+    if(time1==time2 || !time1 || !time2){
+        return alert("Erro na escolha dos times.");
+    }
+
+    for(let jogador of time1.jogadores){
+        jogador.jogando = true;
+    }
+    for(let jogador of time2.jogadores){
+        jogador.jogando = true;
+    }
+    console.log(time1, time2);
+    
+    let registroPartida = simulacaoPartida(document.querySelector("#estilo").value,
+    document.querySelector("#clima").value, 
+    document.querySelector("#torcida").value, 
+    document.querySelector("#moralTime1").value, 
+    document.querySelector("#moralTime2").value,
+    time1, time2);
+
+    limparEventos();
+    exibirTimesPlacar(time1, time2);
+    rodarPartida(registroPartida,  document.querySelector("#velocidade").value, time1, time2);
+}
+
+function rodarPartida(registroPartida, velocidadePartida, time1, time2){
+    let [sumula, tempoPartida] = registroPartida;
+    
+    const eventos = document.querySelector("#eventos");
+    const placarT1 = document.querySelector("#placarTime1");
+    const placarT2 = document.querySelector("#placarTime2");
+    
+    const timer = document.querySelector("#timer");
+    let minuto = 0;
+    
+    const intervalo = setInterval(() => {
+        minuto++;
+        timer.textContent = minuto+"'";
+        
+        for(let evento of sumula){
+            if(evento.minuto==minuto){
+                
+                if(evento.time == time1){
+                    classeEvento = "eventoTime1";
+                }else{
+                    classeEvento = "eventoTime2";
+                }
+                
+                if(evento.tipo=="gol"){
+                    switch(evento.time){
+                        case time1:
+                            placarT1.textContent ++;
+                            break;
+                            case time2:
+                            placarT2.textContent ++;
+                            break;
+                    }
+                    emoji = "⚽";
+                }else if(evento.tipo=="vermelho"){
+                    emoji = "🟥";
+                }
+                
+                eventos.innerHTML += `<div class="${classeEvento}"><p>${minuto}'</p> <p>${emoji}</p> <p>${evento.jogador}</p></div>`;
+            }
+        }
+        
+        if(minuto >= tempoPartida){
+            clearInterval(intervalo);
+            
+            const iniciarPartidaBtn = document.querySelector("#iniciarPartidaBtn");
+            iniciarPartidaBtn.style.pointerEvents = "auto";
+        }
+    }, velocidadePartida);
+
+}
+
+function exibirTimesPlacar(time1, time2){
+    const time1Placar = document.querySelector("#time1Placar");
+    const time2Placar = document.querySelector("#time2Placar");
+    
+    time1Placar.textContent = time1.nome;
+    time1Placar.style.backgroundColor = time1.cor1;
+    time1Placar.style.color = time1.cor2;
+    
+    time2Placar.textContent = time2.nome;
+    time2Placar.style.backgroundColor = time2.cor1;
+    time2Placar.style.color = time2.cor2;
+}
+
+function limparEventos(){
+    document.querySelector("#eventos").innerHTML = "";
+    placarT1 = document.querySelector("#placarTime1").innerHTML = "0";
+    placarT2 = document.querySelector("#placarTime2").innerHTML = "0";
 }
---
+++ js/simulacao.js
@@ -0,0 +1,197 @@
+function simulacaoPartida(estilo, clima, torcida, moralTime1, moralTime2, time1, time2){
+    let [chanceEventoMod, chanceEventoTime1Mod, chanceEventoTime2Mod, chanceVermelhoMod] = calcularModificadores(estilo, clima, torcida, moralTime1, moralTime2);
+    
+    let ataqueTime1 = calcularForca(time1, "atk");
+    let defesaTime1 = calcularForca(time1, "def");
+    let ataqueTime2 = calcularForca(time2, "atk");
+    let defesaTime2 = calcularForca(time2, "def");
+
+    let chanceEvento = 3 + chanceEventoMod;
+    let chanceEventoT1 = Math.max(1, (50 + (ataqueTime1-defesaTime2) * 2) + chanceEventoTime1Mod); 
+    let chanceEventoT2 = Math.max(1, (50 + (ataqueTime2-defesaTime1) * 2) + chanceEventoTime2Mod); 
+    console.log(chanceEventoT1, chanceEventoT2);
+    let chanceVermelho = 5 + chanceVermelhoMod;
+
+    let sumula = [];
+    let tempoPartida = 90 + Math.floor(Math.random()*10);
+    let timeEvento = "";
+    
+    for(let minuto=0;minuto<=tempoPartida;minuto++){
+        let valorRandom = Math.random() * 100;
+
+        if(valorRandom < chanceEvento){
+            timeEvento = sorteiarTimeEvento(time1, time2, chanceEventoT1, chanceEventoT2);
+            jogador = sorteiarJogadorEvento(timeEvento.jogadores);
+            evento = sorteiarTipoEvento(chanceVermelho);
+
+            if(evento=="vermelho"){
+                switch(timeEvento){
+                    case time1:
+                        chanceEventoT1 -= 10;
+                        break;
+                    case time2:
+                        chanceEventoT2 -= 10;
+                        break;
+                    }
+                        
+                    for(let i=0;i<timeEvento.jogadores.length;i++){
+                        if(timeEvento.jogadores[i] == jogador){
+                            timeEvento.jogadores[i].jogando = false;
+                        }
+                    }
+                    if(timeEvento.jogadores.length<=3){
+                        chanceVermelho = 0;
+                    }
+                }
+
+            if(minuto==45 && clima=="quente"){
+                chanceEvento -= 3;
+            }
+            if(minuto==75 && clima=="quente"){
+                chanceEvento -= 1;
+            }
+            
+            sumula.push({minuto: minuto, time: timeEvento, jogador: jogador.nome, tipo: evento});
+        }
+    }
+
+    return [sumula, tempoPartida];
+}
+
+function calcularForca(time, forca){
+    switch (forca){
+        case "atk": 
+            pesos = [0, 0.5, 2, 4];
+            pesoIdeal = 11;
+            break;
+        case "def": 
+        pesos = [3, 4, 1.5, 0.5];
+        pesoIdeal = 13.5;
+        break;
+    }
+    
+    oversEpesos = [];
+    
+    for(let jogador of time.jogadores){
+        switch (jogador.pos){
+            case "GK": peso = pesos[0]; 
+            break;
+            case "ZG": peso = pesos[1];
+            break;
+            case "MC": peso = pesos[2]; 
+            break;
+            case "AT": peso = pesos[3]; 
+            break;
+        }
+        oversEpesos.push([jogador.over, peso]);
+    }
+    
+    let valorFinal = 0;
+    
+    for(let valorEpeso of oversEpesos){
+        let [valor,peso] = valorEpeso;
+        valorFinal += (valor*peso);
+    }
+    
+    return valorFinal / pesoIdeal;
+}
+
+function calcularModificadores(estilo, clima, torcida, moralTime1, moralTime2){
+    let [chanceEventoMod, chanceEventoTime1Mod, chanceEventoTime2Mod, chanceVermelhoMod] = [0, 0, 0, 0];
+    
+    switch(estilo){
+        case "varzeano":
+            chanceEventoMod += -1;
+            break;
+        case "maluco":
+            chanceEventoMod += 2;
+            break;
+        case "ofensivo":
+            chanceEventoMod += 2;
+            break;
+        case "violento":
+            chanceVermelhoMod += 5; 
+            break;
+    }
+            
+    switch(clima){
+        case "quente":
+            chanceEventoMod += 3;
+            break;
+        case "frio":
+            chanceEventoMod += -1.5;
+            break;
+        case "chuvoso":
+            chanceEventoMod += -1;
+            break;
+    }
+            
+    switch(torcida){
+        case "unica":
+            chanceEventoTime1Mod += 4;
+            break;
+        case "meio-a-meio":
+            chanceEventoMod += 1.5;
+            break;
+        case "mandanteForte":
+            chanceEventoTime1Mod += 5;
+            break;    
+        case "visitanteForte":
+            chanceEventoTime2Mod += 5;
+            break;    
+    }
+        
+    switch(moralTime1){
+        case "boa":
+            chanceEventoTime1Mod += 5;
+            break;
+        case "ruim":
+            chanceEventoTime1Mod += -5;
+            break;
+    }
+        
+    switch(moralTime2){
+        case "boa":
+            chanceEventoTime2Mod += 8;
+            break;
+        case "ruim":
+            chanceEventoTime2Mod += -8;
+            break;
+    }
+                                                
+    return [chanceEventoMod, chanceEventoTime1Mod, chanceEventoTime2Mod, chanceVermelhoMod];
+}
+
+function sorteiarTimeEvento(time1, time2, chanceT1, chanceT2){
+    let valorRandom = Math.random() * (chanceT1+chanceT2) + 1;
+    
+    if(valorRandom < chanceT1){
+        timeEvento = time1;
+    }else{
+        timeEvento = time2;
+    }
+    
+    return timeEvento;
+}
+
+function sorteiarJogadorEvento(jogadores){
+    let valorRandom = Math.floor(Math.random() * (jogadores.length-1))+1;
+    let jogador = jogadores[valorRandom];
+
+    if(jogador.jogando){
+        return jogador;
+    }else{
+        sorteiarJogadorEvento(jogadores);
+    }
+}
+
+function sorteiarTipoEvento(chanceVermelho){
+    let valorRandom = Math.random() * 100 + 1;
+    if(valorRandom > chanceVermelho){
+        evento = "gol";
+    }else{
+        evento = "vermelho";
+    }
+
+    return evento;
+}
